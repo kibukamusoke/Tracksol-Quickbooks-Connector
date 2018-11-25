88142d96074a6ecee4098fdd5e8bc7355a558201
Trevor Suna <trevorsuna@gmail.com>
2018-11-25 14:23:25 +0800

    -- initial commit

--- core/quickbooks.js
+++ core/quickbooks.js
@@ -12,6 +12,7 @@ module.exports = {
         return new Promise((resolve, reject) => {
 
             let resString = 'T=7\nI=1\nR=1\nL=CUSTOMER.TXT\nM=1\nD=\n';
+            let resString2 = 'T=7\nI=1\nR=1\nL=CUSTOMER.LST\nM=1\nD=\n';
             fs.readFile(__dirname + '/../config.json', (err, config) => {
                 if (err) {
                     console.log(err);
@@ -43,9 +44,11 @@ module.exports = {
                     console.log(console.log(customers.QueryResponse.Customer[0]));
                     customers.QueryResponse.Customer.forEach((customer, index) => {
                         resString += customer.Id + '|' + encodeURI(customer.FullyQualifiedName) + '|' + customer.Id + '|' + (customer.PrimaryPhone ? customer.PrimaryPhone.FreeFormNumber : '') + '|' + (customer.ShipAddr && customer.ShipAddr.Line1 ? encodeURI(customer.ShipAddr.Line1) : '') + (customer.ShipAddr && customer.ShipAddr.City ? '%0A' + encodeURI(customer.ShipAddr.City) : '') + (customer.ShipAddr && customer.ShipAddr.PostalCode ? '%0A' + customer.ShipAddr.PostalCode : '') + (customer.ShipAddr && customer.ShipAddr.CountrySubDivisionCode ? ' ' + customer.ShipAddr.CountrySubDivisionCode : '') + '|\n';
+                        resString2 += encodeURI(customer.FullyQualifiedName) + '|' + customer.Id + '|\n';
                     });
                     resString = 'B=' + resString.length + '\n' + resString;
-                    resolve(prevString + resString);
+                    resString2 = 'B=' + resString2.length + '\n' + resString2;
+                    resolve(prevString + resString + resString2);
                 });
             });
         });
