9537c9643be6e0d4147443e1f8ed8dbcfeeb913e
Trevor Suna <trevorsuna@gmail.com>
2018-11-25 10:53:08 +0800

    -- initial commit

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ app.get('/update', qb.getProducts);
 
 
 // Start server on HTTP (will use ngrok for HTTPS forwarding)
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 });
